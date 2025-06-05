3b0a800417fabbc3953891b58834df64373fa8a5
Abdul-Manan-PG <manan12345ch@gmail.com>
2025-06-05 11:45:10 +0500

    2_add_two_numbers_modification

--- Leet_Code_Problems/2_Add_Two_Numbers.cpp
+++ Leet_Code_Problems/2_Add_Two_Numbers.cpp
@@ -28,48 +28,77 @@ struct ListNode {
     ListNode(int x) : val(x), next(nullptr) {}
     ListNode(int x, ListNode *next) : val(x), next(next) {}
 };
-#include <string>
-#include <algorithm>
-using namespace std;
- 
+
+
+//#include <string>
+//#include <algorithm>
+//using namespace std;
+// 
+//class Solution {
+//public:
+//    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+//        ListNode* ptr = l1;
+//        string a;
+//        while (ptr != nullptr) {
+//            a += to_string(ptr->val);
+//            ptr = ptr->next;
+//        }
+//        for (int i = 0; i < a.length() / 2; i++) {
+//            char t = a[i];
+//            a[i] = a[a.length() - 1 - i];
+//            a[a.length() - 1 - i] = t;
+//        }
+//        int n1 = stoi(a);
+//
+//        ptr = l2;
+//        string b;
+//        while (ptr != nullptr) {
+//            b += to_string(ptr->val);
+//            ptr = ptr->next;
+//        }
+//        for (int i = 0; i < b.length() / 2; i++) {
+//            char t = b[i];
+//            b[i] = b[b.length() - 1 - i];
+//            b[b.length() - 1 - i] = t;
+//        }
+//        int n2 = stoi(b);
+//
+//        int result = n1 + n2;
+//        ptr = &ListNode(result%10, nullptr);
+//        ListNode* r = ptr;
+//        result /= 10;
+//        while (result != 0) {
+//            ptr->next = &ListNode(result % 10, nullptr);
+//            ptr = ptr->next;
+//            result /= 10;
+//        }
+//        return r;
+//    }
+//};
+
 class Solution {
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        ListNode* ptr = l1;
-        string a;
-        while (ptr != nullptr) {
-            a += to_string(ptr->val);
-            ptr = ptr->next;
-        }
-        for (int i = 0; i < a.length() / 2; i++) {
-            char t = a[i];
-            a[i] = a[a.length() - 1 - i];
-            a[a.length() - 1 - i] = t;
-        }
-        int n1 = stoi(a);
+        ListNode dummy(0);
+        ListNode* current = &dummy;
+        int carry = 0;
 
-        ptr = l2;
-        string b;
-        while (ptr != nullptr) {
-            b += to_string(ptr->val);
-            ptr = ptr->next;
-        }
-        for (int i = 0; i < b.length() / 2; i++) {
-            char t = b[i];
-            b[i] = b[b.length() - 1 - i];
-            b[b.length() - 1 - i] = t;
+        while (l1 != nullptr || l2 != nullptr || carry != 0) {
+            int sum = carry;
+            if (l1 != nullptr) {
+                sum += l1->val;
+                l1 = l1->next;
+            }
+            if (l2 != nullptr) {
+                sum += l2->val;
+                l2 = l2->next;
+            }
+            carry = sum / 10;
+            current->next = new ListNode(sum % 10);
+            current = current->next;
         }
         int n2 = stoi(b);
 
-        int result = n1 + n2;
-        ptr = &ListNode(result%10, nullptr);
-        ListNode* r = ptr;
-        result /= 10;
-        while (result != 0) {
-            ptr->next = &ListNode(result % 10, nullptr);
-            ptr = ptr->next;
-            result /= 10;
-        }
-        return r;
+        return dummy.next;
     }
 };
